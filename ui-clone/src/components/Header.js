44aaa9371da059193a2210af24d473b7a18e3cd4
Sandy Caba <joriel@Sandys-MacBook-Air.local>
2022-10-28 11:36:04 -0400

    added all navbar icons

--- ui-clone/src/components/Header.js
+++ ui-clone/src/components/Header.js
@@ -2,7 +2,11 @@ import React from 'react'
 import "./Header.css"
 import Icon from '@mui/material/Icon';
 import HomeIcon from '@mui/icons-material/Home'
-
+import FlashOnIcon from '@mui/icons-material/FlashOn'
+import LiveTvIcon from '@mui/icons-material/LiveTv'
+import VideoLibraryIcon from '@mui/icons-material/VideoLibrary'
+import SearchIcon from '@mui/icons-material/Search'
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline'
 
 
 function Header() {
@@ -10,6 +14,11 @@ function Header() {
     <div className='header'>
       <div className='header-icons'>
         < HomeIcon />
+        <FlashOnIcon />
+        <LiveTvIcon />
+        <VideoLibraryIcon />
+        <SearchIcon />
+        <PersonOutlineIcon />
       </div>
       <img src='https://suplari.com/wp-content/uploads/2019/09/hulu-logo-white.png' alt=''/>
     </div>
