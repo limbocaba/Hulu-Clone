00fe389dfbe8e0705994b8b77e8888bba52a6bfe
Sandy Caba <joriel@Sandys-MacBook-Air.local>
2022-10-28 18:10:39 -0400

    implemented functionality to nav bar, all clickables working

--- ui-clone/src/App.js
+++ ui-clone/src/App.js
@@ -1,19 +1,22 @@
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Nav from './components/Nav';
 import Results from './components/Results';
-
+import requests from './requests';
 
 function App() {
+const [selectedOption, setSelectedOption] = useState(requests.fetchTrending)
+
   return (
     <div className="app">
 
       <Header />
 
-      <Nav />
+      <Nav setSelectedOption={ setSelectedOption} />
 
-      <Results />
-      
+      <Results selectedOption={selectedOption}/>
+
     </div>
   );
 }
--- ui-clone/src/components/Results.js
+++ ui-clone/src/components/Results.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './Results.css'
-import VideoCard from './VideoCard';
+import  VideoCard  from './VideoCard';
 import axios from '../axios'
 import requests from '../requests';
 
@@ -10,19 +10,19 @@ function Results({ selectedOption }) {
 
   useEffect(() => { // run this code when the Results component loads/mounts
     async function fetchData() {
-      const response = await axios.get(requests.fetchActionMovies)
+      const response = await axios.get(selectedOption)
      
       setMovies(response.data.results)
       return response
     }
     
     fetchData()
-  }, [])
+  }, [selectedOption])
 
   return (
     <div className="results">
       {movies.map((movie) => (
-        <VideoCard movie={movie}/>
+        <VideoCard key={ movie.id } movie={movie}/>
       ))}
       
     </div>
