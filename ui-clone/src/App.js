b4740d0d666c6a4edf4d1fedbb68a5e032659195
Sandy Caba <joriel@Sandys-MacBook-Air.local>
2022-10-28 11:27:40 -0400

    nothing changed in these files

--- ui-clone/src/App.js
+++ ui-clone/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Header from './components/Header';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Lets build a hulu clone </h1>
+
+      <Header />
+
+      {/* Navs */}
+
+      {/* Results */}
     </div>
   );
 }
