e8071b32b4072ab54eef8aebd5ea936a425f69aa
Sandy Caba <joriel@Sandys-MacBook-Air.local>
2022-10-28 15:10:53 -0400

    working api call, no errors

--- ui-clone/src/components/Results.js
+++ ui-clone/src/components/Results.js
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Results.css'
 import VideoCard from './VideoCard';
+import axios from '../axios'
+import requests from '../requests';
 
 function Results() {
+  const [movies, setMovies] = useState([])
+  console.log(requests.fetchActionMovies)
+  useEffect(() => { // run this code when the Results component loads/mounts
+    async function fetchData() {
+      const response = await axios.get(requests.fetchActionMovies)
+     
+      setMovies(response.data.results)
+      return response
+    }
+    
+    fetchData()
+  }, [])
+
   return (
     <div className="results">
-      <VideoCard  />
-      <VideoCard  />
-      <VideoCard  />
-      <VideoCard  />
-      <VideoCard  />
-      <VideoCard  />
-      <VideoCard  />
+      {movies.map((movie) => (
+        <VideoCard movie={movie}/>
+      ))}
+      
     </div>
   )
 }
