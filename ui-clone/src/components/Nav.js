d89d8433eeb45d6895ddf976f33215da9be9dfd2
Sandy Caba <joriel@Sandys-MacBook-Air.local>
2022-10-28 12:46:37 -0400

    fixed navbar font-size, no overlaps now

--- ui-clone/src/components/Nav.js
+++ ui-clone/src/components/Nav.js
@@ -10,7 +10,7 @@ function Nav() {
       <h2>Horror</h2>
       <h2>Romance</h2>
       <h2>Mystery</h2>
-      <h2>Sci-Fi</h2>
+      <h2>Sci-fi</h2>
       <h2>Western</h2>
       <h2>Animation</h2>
       <h2>Movie</h2>
